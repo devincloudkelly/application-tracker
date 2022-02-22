c37a42b99beaa659a4f3dd484e92afeb24089868
devincloudkelly <devin.cloud.kelly@gmail.com>
2022-02-21 21:00:33 -0600

    Refactor className and selectedView state to be more clear

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import ButtonGroup from "./components/ButtonGroup";
 
 function App() {
-  // Need to return a button group at top to switch between adding an app, viewing old apps
   // Then return the content based on which button is selected
   const [selectedView, setSelectedView] = useState("add-new-btn");
 
--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -2,21 +2,24 @@ const ButtonGroup = (props) => {
   let { view, setView } = props;
 
   const handleClick = (e) => {
-    console.log("handling click", e.target.id);
+    console.log("handling click, view, e.target.id", view, e.target.id);
+    if (view !== e.target.id) {
+      setView(e.target.id);
+    }
   };
 
   return (
     <div className="btn-group">
       <button
         id="add-new-btn"
-        className={view ? "selected" : ""}
+        className={view === "add-new-btn" ? "selected" : ""}
         onClick={(e) => handleClick(e)}
       >
         Add New
       </button>
       <button
         id="view-all-btn"
-        className={!view ? "selected" : ""}
+        className={view === "view-all-btn" ? "selected" : ""}
         onClick={(e) => handleClick(e)}
       >
         View All
