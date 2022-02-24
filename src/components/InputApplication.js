af9693898349493ed74cdde27a24a222f55d5667
devincloudkelly <devin.cloud.kelly@gmail.com>
2022-02-23 22:11:08 -0600

    Start basic form for InputApplication, need to update it to React form

--- src/components/InputApplication.js
+++ src/components/InputApplication.js
@@ -1,5 +1,31 @@
 const InputApplication = () => {
-  return <section>Input Application info</section>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handling form submit..", event);
+  };
+
+  return (
+    <article>
+      <form>
+        <label>
+          <p>Salary Range</p>
+        </label>
+        <input name="salary-range" type="text"></input>
+        <label>
+          <p>Notes</p>
+        </label>
+        <input name="notes" type="text"></input>
+        <label>
+          <p>Notes</p>
+        </label>
+        <input name="notes" type="text"></input>
+        <button type="submit" onClick={(event) => handleSubmit(event)}>
+          ADD
+        </button>
+        <p>Click to save the url and date. All fields are optional.</p>
+      </form>
+    </article>
+  );
 };
 
 export default InputApplication;
