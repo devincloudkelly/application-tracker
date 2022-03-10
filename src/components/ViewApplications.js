693ab06a787d0d373e26bde50185e17b617898e3
devincloudkelly <devin.cloud.kelly@gmail.com>
2022-03-09 22:35:40 -0600

    Build basic render view for applications. Apps are not rendering, need to troubleshoot

--- src/components/ViewApplications.js
+++ src/components/ViewApplications.js
@@ -1,5 +1,36 @@
-const ViewApplications = () => {
-  return <section>View Applications</section>;
+const ViewApplications = (props) => {
+  const { applications } = props;
+
+  const renderApplications = () => {
+    console.log("rendering applications...", applications);
+    return applications.forEach((application) => {
+      console.log(application);
+      const { salaryRange, notes, jobTitle, url, date } = application;
+      return (
+        <div className="app-div">
+          <div>
+            <h5>{jobTitle}</h5>
+            <h5>{salaryRange}</h5>
+          </div>
+          <div>
+            <h5>
+              <a href={url} target="_blank" rel="noreferrer">
+                {url}
+              </a>
+            </h5>
+            <h5>{date.toDateString()}</h5>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <article>
+      <h2>View Applications</h2>
+      {renderApplications()}
+    </article>
+  );
 };
 
 export default ViewApplications;
